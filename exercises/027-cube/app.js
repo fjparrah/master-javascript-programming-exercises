c62cb0cde2588b9fbd1c328a83122111679050e0
fjparrah <48214359+fjparrah@users.noreply.github.com>
2024-01-07 22:39:45 +0000

    actualizacion

--- exercises/027-cube/app.js
+++ exercises/027-cube/app.js
@@ -1,1 +1,4 @@
 // Write your function here
+function cube(num){
+    return cubes = num ** 3
+}
--- exercises/028-square/app.js
+++ exercises/028-square/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+
+function square(num){
+    return cuadrado = num * num
+}
